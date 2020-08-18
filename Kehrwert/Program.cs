33561a35f7219c40b7de48310b8847ef629112b1
Nico Bauer <Bauer.Nico1998@outlook.de>
2020-08-18 13:33:16 +0200

    Ändeurng

---
+++ Kehrwert/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Nico Bauer
+/// IS218
+/// AWE
+/// Von einer eingegebenen Zahl soll der Kehrwert ermittelt und dann beide Zahlen ausgegeben werden. 
+/// Beachte, dass bei einer Null die Bildung des Kehrwertes nicht möglich ist! Eine entsprechende Meldung soll auf dem Bildschirm ausgegeben werden. 
+/// </summary>
+
+
+namespace Kehrwert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
